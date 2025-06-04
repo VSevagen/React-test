eb2d8f5f3ca356a990ae703ef22d7023c39e1e01
Rado <radonirina.maminiaina@gmail.com>
2025-06-05 01:07:34 +0300

    feat: search wip

---
+++ src/hooks/useSearch.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const useSearch = data => {
+  const [results, setResults] = useState([]);
+
+  const onSearch = keyword => {
+    if (keyword === "") {
+      setResults(data);
+      return;
+    }
+    const keywordRegex = new RegExp(keyword, "gmi");
+    const foundResults = data.filter(d => {
+      const keys = Object.keys(d);
+      return keys.find(k => keywordRegex.test(d[k]));
+    });
+    setResults(foundResults);
+  };
+
+  return {
+    results,
+    onSearch,
+  };
+};
+
+export default useSearch;
